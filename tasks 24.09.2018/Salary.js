2d49a1f7a7166b1e4690d475d605f044632a00a0
Yauheni Humanau <idracat@gmail.com>
2018-09-25 15:02:08 +0300

    task salary new function
    done

--- tasks 24.09.2018/Salary.js
+++ tasks 24.09.2018/Salary.js
@@ -1,23 +1,30 @@
+// function mathSalary(a, b, c) {
+//     if (a > c && a > b) {
+//         if (c > b) {
+//             return a - b;
+//         } else {
+//             return a - c;
+//         }
+//     } else if (b > a && b > c) {
+//         if (a > c) {
+//             return b - c;
+//         } else {
+//             return b - a;
+//         }
+//     } else if (c > a && c > b) {
+//         if (a > b) {
+//             return c - b;
+//         } else {
+//             return c - a;
+//         }
+//     }
+// }
+//
+// mathSalary(200, 1100, 600);
+
 function mathSalary(a, b, c) {
-    if (a > c && a > b) {
-        if (c > b) {
-            return a - b;
-        } else {
-            return a - c;
-        }
-    } else if (b > a && b > c) {
-        if (a > c) {
-            return b - c;
-        } else {
-            return b - a;
-        }
-    } else if (c > a && c > b) {
-        if (a > b) {
-            return c - b;
-        } else {
-            return c - a;
-        }
-    }
+
+    return Math.max(a, b, c) - Math.min(a, b, c);
 }
 
-mathSalary(200, 1100, 600);
+console.log(mathSalary(150, 2200, 213));

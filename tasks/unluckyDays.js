5240bfae4f632412fef46bbbacc73ddfa16076e8
Yauheni Humanau <idracat@gmail.com>
2019-02-20 12:03:05 +0300

    change loop body for unluckyDays function

--- tasks/unluckyDays.js
+++ tasks/unluckyDays.js
@@ -1,7 +1,7 @@
 function numberOfFridaythe13thsIn(year) {
     let count = 0;
-    
-    for (let month = 12; month > 0; month--) {
+
+    for (let month = 0; month < 12; month++) {
         let d = new Date(year, month, 13);
         if(d.getDay() === 5) {
             count++;

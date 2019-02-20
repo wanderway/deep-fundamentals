81438722dd9d03602c93efa69fbc21675d40aa6d
Yauheni Humanau <idracat@gmail.com>
2019-02-20 11:50:28 +0300

    find numbers of Friday the 13 in year using object new Date() and method .getDay()

---
+++ tasks/unluckyDays.js
@@ -0,0 +1,13 @@
+function numberOfFridaythe13thsIn(year) {
+    let count = 0;
+    
+    for (let month = 12; month > 0; month--) {
+        let d = new Date(year, month, 13);
+        if(d.getDay() === 5) {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(numberOfFridaythe13thsIn(1995)); // 1
